6d679220cbcd7803f9bc58c27f75e833046ef0a3
BlackFlair <surinmachaiah@gmail.com>
2020-06-18 13:02:55 +0530

    QuizViewModelFactory.java

--- app/src/main/java/com/black/flair/quizitup/UI/CustomView/QuizViewModelFactory.java
+++ app/src/main/java/com/black/flair/quizitup/UI/CustomView/QuizViewModelFactory.java
@@ -1,4 +1,25 @@
 package com.black.flair.quizitup.UI.CustomView;
 
-public class QuizViewModelFactory {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class QuizViewModelFactory extends ViewModelProvider.AndroidViewModelFactory {
+
+    private Application application;
+
+    public QuizViewModelFactory(@NonNull Application application){
+
+        super(application);
+        this.application = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass){
+
+        return (T) new QuizViewModel(application);
+    }
 }
