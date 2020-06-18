bf7e5c15ac096deed0038f38ea219d2ea4f3c7bd
BlackFlair <surinmachaiah@gmail.com>
2020-06-18 13:30:43 +0530

    MainActivity.java

--- app/src/main/java/com/black/flair/quizitup/UI/MainActivity.java
+++ app/src/main/java/com/black/flair/quizitup/UI/MainActivity.java
@@ -1,43 +1,90 @@
 package com.black.flair.quizitup.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.black.flair.quizitup.R;
+import com.black.flair.quizitup.UI.CustomView.QuizView;
+import com.black.flair.quizitup.UI.CustomView.QuizViewModel;
+import com.black.flair.quizitup.UI.CustomView.QuizViewModelFactory;
+import com.black.flair.quizitup.data.TaskEntry;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuizView view;
+    private QuizViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this, new QuizViewModelFactory(this.getApplication())).get(QuizViewModel.class);
+        view = findViewById(R.id.quizView);
+
+        viewModel.states.observe(this, new Observer<List<TaskEntry>>() {
+
+            @Override
+            public void onChanged(List<TaskEntry> states) {
+                if (states != null) {
+                    if (states.size() == 4) {
+                        view.setData(states);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Add More states", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        view.setOptionsClickListener(new QuizView.optionsClickListener() {
+            @Override
+            public void OnClicked(Boolean result) {
+                updateResult(result);
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.ic_list:
-                Intent listIntent = new Intent(MainActivity.this, ListRV.class);
-                startActivity(listIntent);
-                break;
-
-            case R.id.ic_settings:
-                Intent settingsIntent = new Intent(MainActivity.this, Settings.class);
-                startActivity(settingsIntent);
-                break;
+    private void updateResult(Boolean result) {
+        if (result) {
+            Toast.makeText(MainActivity.this, "Correct answer", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Wrong answer", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
+        viewModel.refreshGame();
+        view.reset();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_bar_1, menu);
+        inflater.inflate(R.menu.main_activity_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.list_activity:
+                Intent intent = new Intent(MainActivity.this, ListRV.class);
+                startActivity(intent);
+                return true;
+            case R.id.settings_fragment:
+                Intent intent1 = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/com/black/flair/quizitup/Utils/NotificationWorker.java
+++ app/src/main/java/com/black/flair/quizitup/Utils/NotificationWorker.java
@@ -1,4 +1,27 @@
 package com.black.flair.quizitup.Utils;
 
-public class NotificationWorker {
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.black.flair.quizitup.data.TaskEntry;
+import com.black.flair.quizitup.database.StateRepository;
+
+public class NotificationWorker extends Worker {
+    private StateRepository stateRepository;
+    public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        stateRepository = StateRepository.getStateRepository((Application) context.getApplicationContext());
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        TaskEntry state  = stateRepository.getRandomState();
+        Notifications.getDailyNotification(getApplicationContext(),state);
+        return Result.success();
+    }
 }
